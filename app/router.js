695ad582adb6664f418ef945a6f32db312507d7e
Chris Stephens <chrisjstephens@yahoo.com>
2015-04-20 20:36:20 -0400

    Apr 20 Updates
    -Implemented basic templates w/ routies to indicate news headers for
    sites

---
+++ app/router.js
@@ -0,0 +1,7 @@
+App.Router.map(function() {
+  this.route('home', { path: '/' });
+  this.route('local', { path: '/local' });
+  this.route('worldnews', { path: '/worldnews' });
+  this.route('business', { path: '/business' });
+  this.route('entertainment', { path: '/entertainment' });  
+});
