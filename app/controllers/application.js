1141be58daa030f2d4f7646e32f3170cac6496f9
Chris Stephens <chrisjstephens@yahoo.com>
2015-05-04 23:58:56 -0400

    May 4th Update
    After much trial and error I got the Ember Stock Market working with
    ember features, and not pure javascript/jquery.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,28 @@
 App.ApplicationController = Ember.Controller.extend({
 	
-	emberStock: "123",
+	emberStock: '123',
 	breakingnews: false,
 	actions: {
 		bnews: function() {
 			this.set('breakingnews', true);
 		}
+	},
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+	setupController: function(controller){
+			this.stockTicker(controller);
+	},
+	stockTicker: function(controller){
+		var self = this;
+		var price = Math.floor(Math.random() * 50) - 1;
+
+		controller.set('emberStock', price);
+		
+		Em.run.later(function(){
+			self.stockTicker(controller);
+		}, 1000);
 	}
-	
 });
 
 //Gets date using moment.js
